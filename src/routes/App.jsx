3a8d7b7166705639461f25d089b74f5b869be8a8
Felipe <felipe.choi@sou.unifal-mg.edu.br>
2022-11-01 23:06:58 -0300

    styling adjustments.

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -5,6 +5,7 @@ import {Typography} from "@mui/material"
 import {Box} from "@mui/material"
 import {Button} from "@mui/material"
 import "@egjs/react-flicking/dist/flicking.css"
+import {Card, CardContent} from "@mui/material"
 
 //using react router for links and possibly conditional rendering.
 import {
@@ -43,7 +44,7 @@ const customTheme = createTheme(deepmerge(baseTheme,iaçú))
 // })
 
 const featured=[
-	<Box component="img" src="lizard.png" maxwidth="100%" height="auto"></Box>,
+	<Box component="img" src="lizard-white.png" maxwidth="100%" height="auto"></Box>,
 	//<img src="archlinux.svg" ></img>,
 	<Typography align="center">{"unlimited meme works."}</Typography>,
 	<Typography align="center">{"honestly i'm running out of ideas."}</Typography>
@@ -52,9 +53,11 @@ const featured=[
 export function HomePage(){
 	return (
 		<Box sx={{width:"100%", backgroundColor:"background.paper"}}>
-			<Typography variant="h3" component="h1"> Flavoured with Computer Science.</Typography>
-			<Typography variant="body" >based cringe normie chad soy wojak shrigma the attentiveness of a non-attractive human-being depends on few factors when taking into account popular thresholds such as 5-minute staredowns, 10% content memorized or direct eyecontact... you still reading this?</Typography>
-			<Box sx={{bgcolor:"primary.main"}}>threshold</Box>
+			<Box sx={{mx:1}}>
+				<Typography variant="h3" component="h1"> Flavoured with Computer Science.</Typography>
+				<Typography variant="body" >based cringe normie chad soy wojak shrigma the attentiveness of a non-attractive human-being depends on few factors when taking into account popular thresholds such as 5-minute staredowns, 10% content memorized or direct eyecontact... you still reading this?</Typography>
+			</Box>
+			<Card sx={{mx:1}}raised><CardContent>threshold</CardContent></Card>
 			<Box sx={{display:"flex",justifyContent:"center"}}><Typography  variant="body"sx={{}} align={"center"}>centered text?</Typography></Box>
 			<Carrousel delay={2700}>
 				{featured.map((e,i) => <div className="flicking-panel" key={i}><Box sx={{ display:"flex", justifyContent:"center"}}>{e}</Box></div>)}
@@ -76,16 +79,16 @@ export function App() {
 	<ThemeProvider theme={customTheme} >
 		<CssBaseline />
 		<Box sx={{width:"100%",height:"100%"}}>
-			<Box sx={{backgroundColor:"primary.main", color:"background.paper", width:"100%", display:"flex"}}>
+			<Box sx={{backgroundColor:"primary.main", color:"background.paper", width:"100%", display:"flex",px:1}}>
 				<Box component="img"
-					src="lizard-white.png"
-					sx={{height:"34px",p:1}}></Box>
+					src="lizard.png"
+					sx={{height:"41px",py:0.31}}></Box>
 				<Button variant="text"
 					sx={{color:"background.paper"}}
 					onClick={() => setContent(<HomePage />)}
 				>	home
 				</Button>
-				<Link><Button variant="text"
+				<Link style={{}}><Button variant="text"
 					sx={{color:"background.paper",maxwidth:"28%"}}
 					onClick={() => setContent(<Academic />)}
 				>	academic media
